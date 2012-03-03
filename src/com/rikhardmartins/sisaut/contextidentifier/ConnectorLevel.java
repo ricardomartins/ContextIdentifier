64b42a0bcac48ff8b76ab576238ac7b4750510b6
Hugo Ricardo Gonçalves Martins <rikhard.martins@gmail.com>
2012-03-03 21:50:06 +0000

    Some code added.

--- src/com/rikhardmartins/sisaut/contextidentifier/ConnectorLevel.java
+++ src/com/rikhardmartins/sisaut/contextidentifier/ConnectorLevel.java
@@ -3,21 +3,38 @@ package com.rikhardmartins.sisaut.contextidentifier;
 public class ConnectorLevel {
 	private int aboveConnections;
 	private int belowConnections;
-	private float array[];
-	
-	public int getAboveConnections(){
+	private float connections[][];
+
+	public ConnectorLevel() {
+		aboveConnections = 0;
+		belowConnections = 0;
+	}
+
+	public int getAboveConnections() {
 		return aboveConnections;
 	}
 
-	public int getBelowConnections(){
+	public int getBelowConnections() {
 		return belowConnections;
 	}
-	
-	public int registerAboveConnector(NeuralCell theCell){
+
+	public int registerAboveConnector(NeuralCell theCell) {
+		if (((aboveConnections + 1) * belowConnections) > 0) {
+			float oneArray[][] = new float[aboveConnections + 1][belowConnections];
+			for (int i = 0; i < aboveConnections; i++)
+				for (int j = 0; j < belowConnections; j++)
+					oneArray[i][j] = connections[i][j];
+			
+		}
+		aboveConnections++;
+		return (aboveConnections - 1);
+	}
+
+	public int registerBelowConnector(NeuralCell theCell) {
 		return 0;
 	}
 
-	public int registerBelowConnector(NeuralCell theCell){
+	public int generateRandomValues() {
 		return 0;
 	}
 }
